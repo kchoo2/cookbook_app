ed9d69b6466235871b54317ea2ac93e2d22188f4
kchoo2 <choo.keith@gmail.com>
2018-09-02 02:45:46 -0500

    update commit

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,8 +1,44 @@
 class Api::RecipesController < ApplicationController
 
-  def one_recipe_action
-    @recipe = Recipe.first
-    render "one_recipe.json.jbuilder"
+  def index
+    @recipes = Recipe.all
+    render 'one_recipe.json.jbuilder'
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def create
+    @recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        prep_time: params[:prep_time],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+    @recipe.save
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    
+    @recipe.save
+
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+
+    render json: {message: "Poof!"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/one_recipe_url" => 'recipes#one_recipe_action'
+    get "/recipes" => 'recipes#index'
+    get "/recipes/:id" => 'recipes#show'
+    post "/recipes" => 'recipes#create'
+    patch "/recipes/:id" => 'recipes#update'
+    delete "/recipes/:id" => 'recipes#destroy'
   end
 end
 
